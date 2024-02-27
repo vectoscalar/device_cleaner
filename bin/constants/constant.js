27c3d31e06efa93a56b4638950746e0904af9f24
manojfulara-vs <manoj.fulara@vectoscalar.com>
2024-02-27 19:12:34 +0530

    code refactoring

--- bin/constants/constant.js
+++ bin/constants/constant.js
@@ -5,7 +5,3 @@ export const OS_TYPES = {
 };
 
 export const CLINAME = "DEVICE_CLEANER";
-export const DELETION_OPTION = {
-  PRE_DEFINED_PATHS: "PreDefinedPaths",
-  CUSTOM_PATHS: "CustomPaths",
-};
